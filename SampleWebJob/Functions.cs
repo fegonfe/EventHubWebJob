1aef747d6ab8178bf24c68ea499fce3a2edaaa92
Fernando Ferreira <fernandogferreira@hotmail.com>
2019-06-14 13:00:23 -0300

    first version

---
+++ SampleWebJob/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SampleWebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void Trigger([EventHubTrigger("hub-private")] EventData message)
+        {
+            string data = Encoding.UTF8.GetString(message.GetBytes());
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Message received. Data: '{data}'");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ SampleWebJob/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Azure.WebJobs.Logging;
+using System.Configuration;
+
+namespace SampleWebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            string eventHubName = ConfigurationManager.AppSettings["EventHubName"];
+            string eventHubConnectionString = ConfigurationManager.AppSettings["EventHubConnectionString"];
+
+            var eventHubConfig = new EventHubConfiguration();
+            
+            eventHubConfig.AddReceiver(eventHubName, eventHubConnectionString);
+
+            config.UseEventHub(eventHubConfig);
+
+            using (var host = new JobHost(config))
+            { 
+                // The following code ensures that the WebJob will be running continuously
+                host.RunAndBlock();
+            }
+        }
+    }
+}
